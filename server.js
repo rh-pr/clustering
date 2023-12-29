c117d078e99b39e36c39a32ea26db64a08a2cb4e
rh-pr <ruslana.hural.j@gmail.com>
2023-12-29 02:57:17 +0200

    reload server with pm2 command

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require ('express');
-const cluster = require ('cluster');
-const os = require ('os');
 
 const app = express ();
 
@@ -16,17 +14,10 @@ app.get ('/', (req, res) => {
 });
 
 app.get ('/timer', (req, res) => {
-  deley (5000);
+  deley (8000);
   res.send (`Get response with delay ${process.pid}`);
 });
 
-if (cluster.isMaster) {
-  console.log ('Master has been started...');
-  const workersNumber = os.cpus ().length;
-  for (let i = 0; i < workersNumber; i++) {
-    cluster.fork ();
-  }
-} else {
-  console.log ('Fork has been started ...');
-  app.listen (3000);
-}
+console.log ('Master has been started...');
+console.log ('Fork has been started ...');
+app.listen (3000);
